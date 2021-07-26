5c1b8f872faf383e38b16fdad9df57241b607ae8
kroim <wonderfulplayer@outlook.com>
2021-07-26 09:06:55 +0800

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { createWeb3ReactRoot, Web3ReactProvider } from '@web3-react/core'
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, DefaultTheme } from 'styled-components';
+import { Provider } from 'react-redux'
+import usePersistedState from './utils/usePersistedState';
+
+import Routes from './routes';
+import store from './state'
+
+import GlobalStyle from './styles/global';
+import { CustomGlobalStyles } from './styles/custom-global';
+import dark from './styles/theme/dark';
+
+import getLibrary from './utils/getLibrary';
+const Web3ProviderNetwork = createWeb3ReactRoot('NETWORK');
+
+const App: React.FC = () => {
+  const [theme,] = usePersistedState<DefaultTheme>('theme', dark);
+
+  return (
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <Web3ProviderNetwork getLibrary={getLibrary}>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <BrowserRouter>
+              <GlobalStyle />
+              <CustomGlobalStyles />
+              <Routes />
+            </BrowserRouter>
+          </ThemeProvider>
+        </Provider>
+      </Web3ProviderNetwork>
+    </Web3ReactProvider>
+  );
+};
+
+export default App;
