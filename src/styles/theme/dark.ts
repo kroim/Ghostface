d0648cb80e2ff2ec651154f94c9fa57f4445c5e8
kroim <wonderfulplayer@outlook.com>
2021-07-29 08:53:23 +0800

    Create dark.ts

---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,21 @@
+export default {
+  title: 'dark',
+
+  colors: {
+    primary: '#FFCC73',
+    secondary: '#717894',
+
+    backgroundPrimary: '#000000',
+    backgroundSecondary: '#ffffff',
+
+    inputBg: '#202020',
+    placeholder: '#999999',
+
+    theme: '#DFE3EA',
+    themeBg: '#384A7F',
+
+    text: '#ffffff',
+
+    transparent: 'rgba(255, 255, 255, 0)',
+  },
+};
