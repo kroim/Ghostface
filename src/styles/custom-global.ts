bf79b556ee8289b497aa7388680543870d4c451b
kroim <wonderfulplayer@outlook.com>
2021-07-29 08:54:11 +0800

    Create custom-global.ts

---
+++ src/styles/custom-global.ts
@@ -0,0 +1,10 @@
+import {createGlobalStyle} from "styled-components"
+
+export const CustomGlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: CartooNature;
+    font-style: normal;
+    font-weight: 700;
+    src: url('/fonts/CartooNatureRegular.woff2') format('woff2');
+  }
+`
