cdafd020b6047842c2d9ce4ddf95eb11da43bc14
kroim <wonderfulplayer@outlook.com>
2021-07-29 08:53:20 +0800

    Create light.ts

---
+++ src/styles/theme/light.ts
@@ -0,0 +1,21 @@
+export default {
+  title: 'light',
+
+  colors: {
+    primary: '#FFCC73',
+    secondary: '#3E4356',
+
+    backgroundPrimary: '#FFFFFF',
+    backgroundSecondary: '#000000',
+
+    inputBg: '#F5F5F5',
+    placeholder: '#999999',
+
+    theme: '#F9F288',
+    themeBg: '#7BB0DE',
+
+    text: '#333333',
+
+    transparent: 'rgba(255, 255, 255, 0)',
+  },
+};
