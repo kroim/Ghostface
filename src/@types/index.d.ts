1a0f349fbe67c200e183fa633d641388d5ba131f
kroim <wonderfulplayer@outlook.com>
2021-08-02 07:00:27 +0800

    Create index.d.ts

---
+++ src/@types/index.d.ts
@@ -0,0 +1,12 @@
+declare module '*.svg';
+declare module '*.png';
+declare module "*.woff2";
+
+// interface Window {
+//     ethereum?: {
+//         isMetaMask?: true
+//         on?: (...args: any[]) => void
+//         removeListener?: (...args: any[]) => void
+//     }
+//     web3?: {}
+// }
